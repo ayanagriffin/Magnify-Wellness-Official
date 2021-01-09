4830bba899c1b5b15edaa4cfea31765263ce8f32
LabibUW <labibj@uw.edu>
2021-01-08 19:16:02 -0800

    Added CSS to events/loggedIn

--- events/events.js
+++ events/events.js
@@ -5,9 +5,9 @@ const events = document.querySelector("#events");
 
 eventsRef.orderBy("timestamp", "asc").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        events.innerHTML += "<p> <span style='color:blue'>" + doc.data().title + " </span>" + "<span>" + doc.data().date + " </span>" + 
-        "<span>" + doc.data().time + " </span>" + "<span>" + doc.data().location + " </span>" + "<span>" + doc.data().description + " </span>" + "<span>" 
-        + doc.data().link + " </span></p>";
+        events.innerHTML += "<p style='text-align: left; padding-left: 550px;'> <br><span>" + doc.data().title + " </span> <br> <img src='./images/pin.svg' style='width: 15px; height: 20px; position: relative; top: 4px;'>" + "<span style='font-family: 'Montserrat', sans-serif; font-weight: 8;'>" + doc.data().location + " </span>" + 
+        "<span> <img src='./images/clock.svg' style='width: 15px; height: 20px; position: relative; top: 4px;'> " + doc.data().date + " </span>" + "<span>" + doc.data().time + " </span> <br>" + "<span>" + doc.data().description + " </span> <br>" + "<span> Register Here: " 
+        + doc.data().link + "<button type='submit' onclick='document.location=\"" + doc.data().link + "\"'>Register</button> </span><br><br></p> <hr class='beautiful-line'>";
         console.log(doc.id, " => ", doc.data());
     
     });
